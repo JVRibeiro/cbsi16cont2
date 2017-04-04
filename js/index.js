5e9c533ede65961e6a23a16c57095651c8dfbd6f
Livrofilia Developers <livrofilia@hotmail.com>
2017-04-04 16:21:07 -0300

    *

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@
   var
 
   primeiraEntrada = true,
+  newRow,
 
   // Cria variaveis para o controle de datas
   date = new Date(),
@@ -58,25 +59,40 @@
         data.saldo.vTo += Number( entrSaida_qtd.value ) * Number( entrSaida_vUn.value );
         data.saldo.vUn = data.saldo.vTo / data.saldo.qtd;
 
-        qtd_total.innerHTML = data.saldo.qtd;
-        vUn_total.innerHTML = moneyFix(data.saldo.vUn);
-        valor_total.innerHTML = moneyFix(data.saldo.vTo);
-
         if ( localStorage['primeiraEntrada'] === 'false'
           && $( '#entrSaida_tipo option' ).size() === 1 )
         {
           $( '#entrSaida_tipo' ).append('<option value=\'saida\'>Saída</option>');
         }
 
-        $( '#itens' ).append(
-            '<li style=\'background-color: #dafff0;\'>'
-            + 'Tipo: <strong>' + entrSaida_tipo.value + '</strong><br>'
-            + 'Data: <strong>' + d + '/' + m + '/' + y + '</strong><br>'
-            + 'Quantidade: <strong>' + Number( entrSaida_qtd.value ) + '</strong><br>'
-            + 'Valor Unitário: <strong><span class=\'money_value\'>' + moneyFix(Number( entrSaida_vUn.value )) + '</span></strong><br>'
-            + 'Valor Total: <strong><span class=\'money_value\'>'  + moneyFix((Number( entrSaida_qtd.value ) * Number( entrSaida_vUn.value ))) + '</span></strong><br>'
-          + '</li>'
-        );
+
+
+        newRow = [
+          d+'/'+m+'/'+y,
+          Number(entrSaida_qtd.value),
+          '<span class=\'money_value\'>'+moneyFix(Number(entrSaida_vUn.value))+'</span>',
+          '<span class=\'money_value\'>'+moneyFix((Number(entrSaida_qtd.value)*Number(entrSaida_vUn.value)))+'</span>',
+          '',
+          '',
+          '',
+          data.saldo.qtd,
+          '<span class=\'money_value\'>'+moneyFix(data.saldo.vUn)+'</span>',
+          '<span class=\'money_value\'>'+moneyFix(data.saldo.vTo)+'</span>',
+        ];
+
+        for ( var i = 0; i < newRow.length; i++ )
+        {
+          if (newRow[i] != '')
+          {
+            $('.output_table > tbody:last-child')
+              .append('<td style="background-color: #deffdf !important;">'+newRow[i]+'</td>');
+          }
+          else
+          {
+            $('.output_table > tbody:last-child')
+              .append('<td style="background-color: #fff !important;">—</td>');
+          }
+        }
 
         replaceComma();
 
@@ -90,12 +106,6 @@
         {
           $( '#entrSaida_tipo' ).append('<option value=\'saida\'>Saída</option>');
         }
-
-        console.log(
-          'Entrada:\nQuantidade: ' + data.saldo.qtd + '\n'
-        + 'Valor unitário: ' + data.saldo.vUn + '\n'
-        + 'Valor Total: ' + data.saldo.vTo + '\n----------------'
-         );
       }
       else
       if ( entrSaida_tipo.value === 'saida'
@@ -108,19 +118,32 @@
         data.saldo.vTo -= Number( entrSaida_qtd.value ) * Number( entrSaida_vUn.value );
         data.saldo.vUn = data.saldo.vTo / data.saldo.qtd;
 
-        qtd_total.innerHTML = data.saldo.qtd;
-        vUn_total.innerHTML = moneyFix(data.saldo.vUn);
-        valor_total.innerHTML = moneyFix(data.saldo.vTo);
+        newRow = [
+          d+'/'+m+'/'+y,
+          '',
+          '',
+          '',
+          Number(entrSaida_qtd.value),
+          '<span class=\'money_value\'>'+moneyFix(Number(entrSaida_vUn.value))+'</span>',
+          '<span class=\'money_value\'>'+moneyFix((Number(entrSaida_qtd.value)*Number(entrSaida_vUn.value)))+'</span>',
+          data.saldo.qtd,
+          '<span class=\'money_value\'>'+moneyFix(data.saldo.vUn)+'</span>',
+          '<span class=\'money_value\'>'+moneyFix(data.saldo.vTo)+'</span>',
+        ];
 
-        $( '#itens' ).append(
-            '<li style=\'background-color: #ffdada;\'>'
-            + 'Tipo: <strong>' + entrSaida_tipo.value + '</strong><br>'
-            + 'Data: <strong>' + d + '/' + m + '/' + y + '</strong><br>'
-            + 'Quantidade: <strong>' + Number( entrSaida_qtd.value ) + '</strong><br>'
-            + 'Valor Unitário: <strong><span class=\'money_value\'>' + moneyFix(Number( entrSaida_vUn.value )) + '</span></strong><br>'
-            + 'Valor Total: <strong><span class=\'money_value\'>'  + moneyFix((Number( entrSaida_qtd.value ) * Number( entrSaida_vUn.value ))) + '</span></strong><br>'
-          + '</li>'
-        );
+        for ( var i = 0; i < newRow.length; i++ )
+        {
+          if (newRow[i] != '')
+          {
+            $('.output_table > tbody:last-child')
+              .append('<td style="background-color: #ffbfbf !important;">'+newRow[i]+'</td>');
+          }
+          else
+          {
+            $('.output_table > tbody:last-child')
+              .append('<td style="background-color: #fff !important;">—</td>');
+          }
+        }
 
         replaceComma();
 
@@ -128,16 +151,11 @@
         window.localStorage.setItem( 'primeiraEntrada', 'false');
         window.localStorage.setItem( 'produtos_cadastrados', $( '#itens' ).html() );
 
+        // Cria a opção Saída
         if ( $( '#entrSaida_tipo option' ).size() === 1 )
         {
           $( '#entrSaida_tipo' ).append('<option value=\'saida\'>Saída</option>');
         }
-
-        console.log(
-          'Saída:\nQuantidade: ' + data.saldo.qtd + '\n'
-        + 'Valor unitário: ' + data.saldo.vUn + '\n'
-        + 'Valor Total: ' + data.saldo.vTo + '\n----------------'
-         );
       }
       else
       {
@@ -149,11 +167,13 @@
   };
 
 
+  // Corrige a string de data de '2017-4-4' para '2017-04-04'
+  m += 1; if ( m < 10 ) {m = 0 + '' + m}; if ( d < 10 ) {d = 0 + '' + d};
+  entrSaida_data.value = y + '-' + m + '-' + d;
 
-  m += 1; if ( m < 10 ) {m = 0 + '' + m}; entrSaida_data.value = y + '-' + m + '-' + d;
 
-
-  setInterval(function ()
+  // Atualiza o relógio
+  window.setInterval(function ()
   {
     var nD = new Date();
     document.querySelector( '#currentTime' )
@@ -162,13 +182,11 @@
 
 
 
-
+  // Event Listeners
   $( '#_cadastrar' ).on( 'click', function ()
   {
     data._cadastrar();
   });
-
-
 
   $( '#menu_btn' ).on( 'click', function ()
   {
@@ -192,8 +210,6 @@
     }
   });
 
-
-
   $( '#menu ul li' ).on( 'click', function ()
   {
     $( '#menu_btn' )
@@ -212,6 +228,8 @@
 
 
 
+
+
 $( document ).ready(function ()
 {
   if ( localStorage['saldo_final'] )
@@ -223,7 +241,7 @@ $( document ).ready(function ()
   {
     $( '#entrSaida_tipo' ).append('<option value=\'saida\'>Saída</option>');
 
-    $( '#itens' ).append(
+    $( '#itens' ).html('').append(
         window.localStorage.getItem( 'produtos_cadastrados' )
     );
   }
@@ -233,26 +251,24 @@ $( document ).ready(function ()
     $( '#itens' ).animate({'scrollTop': $( '#itens' )[0].scrollHeight}, 'slow');
   },1000);
 
-  qtd_total.innerHTML = data.saldo.qtd;
-  vUn_total.innerHTML = moneyFix(data.saldo.vUn);
-  valor_total.innerHTML = moneyFix(data.saldo.vTo);
 
   replaceComma();
 });
 
 
-function moneyFix (val)
-{
-  var x = Number(Math.round(val + 'e2') + 'e-2').toFixed(3);
-  return x;
-}
 
+// Funções
+  // Arredonda o número e exibe 3 casas decimais
+  function moneyFix (val)
+  {
+    var x = Number(Math.round(val + 'e2') + 'e-2').toFixed(3);
+    return x;
+  }
 
-function replaceComma ()
-{
-  var itens = [document.querySelector('#itens') ,document.querySelector('#vUn_total'), document.querySelector('#valor_total')];
-
-  itens[0].innerHTML = itens[0].innerHTML.replace(/(\d)\.(\d)/gi, '$1,$2');
-  itens[1].innerHTML = itens[1].innerHTML.replace(/(\d)\.(\d)/gi, '$1,$2');
-  itens[2].innerHTML = itens[2].innerHTML.replace(/(\d)\.(\d)/gi, '$1,$2');
-}
+  // Adiciona pontos e virgula
+  function replaceComma ()
+  {
+    var itens = [document.querySelector('#itens')];
+    itens[0].innerHTML = itens[0].innerHTML.replace(/(\d)\.(\d)/gi, '$1,$2');
+    itens[0].innerHTML = itens[0].innerHTML.replace(/(\d)(\d)(\d)(\d)\,(\d)/gi, '$1.$2$3$4,$5');
+  }
